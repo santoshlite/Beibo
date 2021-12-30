fe4fc093107b7e88008b2f83ba2cbda92fa7b179
Santosh <61618641+ssantoshp@users.noreply.github.com>
2021-12-30 12:41:40 +0100

    🌕 Swag setup updated

--- setup.py
+++ setup.py
@@ -4,14 +4,14 @@ with open("README.md", "r", encoding="utf8") as fh:
     long_description = fh.read()
 
 setup(
-    name='the_oracle',
-    version='0.1.2',
+    name='beibo',
+    version='0.1.3',
     description='🤖 Predict the stock market with AI 用AI预测股票市场',
-    py_modules=['the_oracle'],
+    py_modules=['beibo'],
     package_dir={'': 'src'},
     long_description=long_description,
     long_description_content_type="text/markdown",
-    url='https://github.com/ssantoshp/Oracle',
+    url='https://github.com/ssantoshp/Beibo',
     author="Santosh Passoubady",
     author_email="santoshpassoubady@gmail.com",
     license='MIT',
